55d1810495f8434db4a78647efc9f2654a827932
BHrrera <angel0cero0@gmail.com>
2024-04-14 23:03:19 -0600

    Primeros ajustes

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ app = Flask(__name__)
 # Configuración de la conexión a MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'med_sys'
+app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_DB'] = 'medsys'
 
 # Inicialización de la extensión MySQL
 mysql = MySQL(app)
+# Configuración para la carpeta estática
+app.static_folder = 'static'
 
 @app.route('/')
 def index():
@@ -22,11 +24,30 @@ def index():
 @app.route('/login', methods=['POST'])
 def login():
     # Aquí irá la lógica para verificar las credenciales de inicio de sesión
+    # Si las credenciales son válidas, redirigir a la página de inicio
+    # De lo contrario, redirigir de nuevo al formulario de inicio de sesión con un mensaje de error
     return redirect(url_for('home'))
 
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/doctor/CRUD')
+def dCRUD():
+    return render_template('CRUD.html')
+
+@app.route('/admin/CRUD')
+def aCRUD():
+    return render_template('CRUD.html')
+
+@app.route('/secretaria/CRUD')
+def sCRUD():
+    return render_template('CRUD.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
